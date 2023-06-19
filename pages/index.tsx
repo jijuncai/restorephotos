c8e63627faad7dbb99fd8718fcb5253c801eef24
Jimmy Business <jimmybusiness@jijuns-MacBook-Pro.local>
2023-06-18 17:36:30 -0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,90 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import SquigglyLines from "../components/SquigglyLines";
+import { Testimonials } from "../components/Testimonials";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Head>
+        <title>Face Photo Restorer</title>
+      </Head>
+
+      <Header />
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
+        <a
+          // href="https://twitter.com/nutlope/status/1626074563481051136"
+          target="_blank"
+          rel="noreferrer"
+          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
+        >
+          超过 <span className="font-semibold">200,000</span> 用户使用我们的产品
+        </a>
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
+        使用AI{" "}
+          <span className="relative whitespace-nowrap text-[#3290EE]">
+            <SquigglyLines />
+            <span className="relative">为每一个人</span>
+          </span >{" "}
+          恢复老照片
+        </h1>
+
+        <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
+        有旧的和模糊的面部照片吗？ 让我们的 AI 恢复它们，这样那些
+           记忆可以继续存在。 100% 免费 – 立即恢复您的照片。
+        </p>
+        <div className="flex justify-center space-x-4">
+          <a
+            className="bg-white rounded-xl text-black font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-gray-100 border"
+            href="https://youtu.be/FRQtFDDrUXQ"
+            target="_blank"
+            rel="noreferrer"
+          >
+            了解它是如何构建的
+          </a>
+
+          <Link
+            className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
+            href="/restore"
+          >
+            恢复您的照片
+          </Link>
+        </div>
+        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+          <div className="flex flex-col space-y-10 mt-4 mb-16">
+            <div className="flex sm:space-x-2 sm:flex-row flex-col">
+              <div>
+                <h2 className="mb-1 font-medium text-lg">原始照片</h2>
+                <Image
+                  alt="Original photo of my bro"
+                  src="/michael.jpg"
+                  className="w-96 h-96 rounded-2xl"
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="sm:mt-0 mt-8">
+                <h2 className="mb-1 font-medium text-lg">恢复后的照片</h2>
+                <Image
+                  alt="Restored photo of my bro"
+                  width={400}
+                  height={400}
+                  src="/michael-new.jpg"
+                  className="w-96 h-96 rounded-2xl sm:mt-0 mt-2"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
